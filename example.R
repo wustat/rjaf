171fefbd98e92b37133badce3fb9b6f61aed3fc6
wustat <wenbowu@umich.edu>
2021-10-06 15:52:36 -0400

    update example.R

--- example.R
+++ example.R
@@ -6,18 +6,30 @@ library(readr)
 library(tidyr)
 library(randomForest)
 library(ranger)
-
-source("library.R")
-Rcpp::sourceCpp("library.cpp")
+library(git2r)
+name_ssh_key <- "id_rsa"
+creds <- cred_ssh_key(ssh_path(paste0(name_ssh_key, ".pub")),
+                      ssh_path(name_ssh_key))
+url.remote <- "git@github.com:wustat/dof.git"
+relpath2local <- "your/path/to/dof"
+abspath2local <- file.path(Sys.getenv("HOME"), relpath2local)
+if (dir.exists(path2local)) {
+  git2r::pull(path2local, creds)
+} else {
+  clone(url.remote, path2local, credentials=creds)
+}
+source(file.path(path2local, "library.R"))
+Rcpp::sourceCpp(file.path(path2local,"library.cpp"))
 n <- 10000; K <- 9; gamma <- 10; sigma <- 10
 id <- "id"; trts <- as.character(0:9); y <- "Y"; trt <- "trt"
 vars <- paste0("X", 1:3)
 lambda <- 0.5; prob <- "prob"; ipw <- T
 ntree <- 1000; ntrt <- 5; nvar <- 3
-set.seed(123)
+set.seed(1)
 data.validation <- sim.data(n, K, gamma, sigma)
 data.trainest <- sim.data(n=500, K, gamma, sigma)
 # check default arguments of growForest for details
+# on iMac (3.6GHz 8-Core Intel Core i9, 16GB, 2019)
 system.time(forest.reg <-
               growForest(data.trainest, data.validation, y, id, trt,
                          vars, prob, ntree=ntree, ntrt=ntrt, nvar=nvar, reg=T))
