8b1310cdcc1bb5d6c0dc5bf8ff1e5f448df2e25c
wustat <wenbowu@umich.edu>
2021-12-23 12:34:42 -0500

    enable clus.tree.growing and clus.outcome.avg

--- library.R
+++ library.R
@@ -17,6 +17,40 @@ sim.data <- function(n, K, gamma, sigma, prob=rep(1,K+1)/(K+1)) {
                 across(c(id, trt), as.character)))
 }
 
+sim.data <- function(n, K, gamma, sigma, count=rep(1,K+1)) {
+  # K: number of clusters
+  options(stringsAsFactors=F)
+  data <- left_join(data.frame(id=1:n,
+                               cl=sample(0:K, n, T, count),
+                               cid=0,
+                               MASS::mvrnorm(n, rep(0,3), diag(3))),
+                    data.frame(cl=0:K, prob=1/sum(count)), by="cl")
+  invisible(sapply(0:K, function(t) data[data$cl==t, "cid"] <<-
+                     sample(count[t+1], sum(data$cl==t), T, rep(1, count[t+1]))))
+  data <- data %>%
+    mutate(tmp1=10+20*(X1>0)-20*(X2>0)-40*(X1>0&X2>0)+1,
+           tmp2=gamma*(2*(X3>0)-1)/(K-1),
+           tmp3=-X1^2,
+           Y=tmp1+tmp2*(cl>0)*(2*cl-K-1)+tmp3*(cl==0)+rnorm(n,0,sigma),
+           trt=str_c("c", cl, "t", cid))
+  mapping <- data %>% distinct(trt, .keep_all=T) %>%
+    select(c(cl, cid, trt)) %>% arrange(trt)
+  # Y: observed outcomes
+  Y.cf.trt <- data.frame(sapply(mapping %>% pull(trt), function(t) {
+    # counterfactural outcomes
+    clus <- mapping %>% filter(trt==t) %>% pull(cl)
+    mutate(data, Y=tmp1+tmp2*(clus>0)*(2*clus-K-1)+tmp3*(clus==0))$Y
+  }))
+  names(Y.cf.trt) <- paste0("Y", mapping %>% pull(trt))
+  Y.cf.cl <- data.frame(sapply(0:K, function(t) {
+    # counterfactural outcomes
+    mutate(data, Y=tmp1+tmp2*(t>0)*(2*t-K-1)+tmp3*(t==0))$Y
+  }))
+  names(Y.cf.cl) <- paste0("Y",0:K)
+  return(mutate(bind_cols(dplyr::select(data, -c(tmp1,tmp2,tmp3)), Y.cf.trt, Y.cf.cl),
+                across(c(id, cl), as.character)))
+}
+
 residualize <- function(data, y, vars, nfold=5, fun.rf="ranger") {
   data$fold <- sample(1:nfold, NROW(data), T, rep(1, nfold))
   if (fun.rf=="randomForest") {
@@ -49,8 +83,7 @@ growForest <- function(data.trainest, data.validation, y, id, trt, vars, prob,
   data.trainest <- mutate(data.trainest, across(c(id, trt), as.character))
   data.validation <- mutate(data.validation, across(c(id, trt), as.character))
   if (resid) data.trainest <- residualize(data.trainest, y, vars, nfold)
-  clustering <- any(clus.tree.growing, clus.outcome.avg)
-  if (clustering) {
+  if (clus.tree.growing) {
     if (ncluster>length(trts)) stop("Invalid ncluster!")
     data.trainest$fold <- sample(1:nfold, NROW(data.trainest), T, rep(1, nfold))
     cluster <-
@@ -67,7 +100,7 @@ growForest <- function(data.trainest, data.validation, y, id, trt, vars, prob,
                        as.matrix(select(data.onefold, all_of(vars))),
                        ntrt, nvar, lambda1, lambda2, ipw, nodesize, ntree,
                        prop.train, epi, reg, impute, setseed, seed)$Y.cf
-      }))), ncluster, nstart=25)$cluster
+      }))), ncluster, nstart=5)$cluster
     df <- data.frame(cluster)
     df[trt] <- as.character(trts)
     df <- summarise(group_by(data.trainest, trt),
@@ -78,22 +111,22 @@ growForest <- function(data.trainest, data.validation, y, id, trt, vars, prob,
     data.trainest <- inner_join(data.trainest, df, by=trt) %>%
       mutate(cluster=as.character(cluster))
     clus <- unique(pull(data.trainest, cluster))
-  }
-  if (clus.tree.growing) {
     str.tree.growing <- as.integer(factor(pull(data.trainest, cluster),
                                           as.character(clus)))
     prob.tree.growing <- pull(data.trainest, prob_cluster)
     nstr <- ncluster
+    if (clus.outcome.avg) {
+      str.outcome.avg <- as.integer(factor(pull(data.trainest, cluster),
+                                           as.character(clus)))
+    } else {
+      str.outcome.avg <- as.integer(factor(pull(data.trainest, trt),
+                                           as.character(trts)))
+    }
   } else {
     str.tree.growing <- as.integer(factor(pull(data.trainest, trt),
                                           as.character(trts)))
     prob.tree.growing <- pull(data.trainest, prob)
     nstr <- ntrt
-  }
-  if (clus.outcome.avg) {
-    str.outcome.avg <- as.integer(factor(pull(data.trainest, cluster),
-                                         as.character(clus)))
-  } else {
     str.outcome.avg <- as.integer(factor(pull(data.trainest, trt),
                                          as.character(trts)))
   }
@@ -104,7 +137,7 @@ growForest <- function(data.trainest, data.validation, y, id, trt, vars, prob,
                    as.matrix(select(data.validation, all_of(vars))),
                    nstr, nvar, lambda1, lambda2, ipw, nodesize, ntree,
                    prop.train, epi, reg, impute, setseed, seed)
-  if (clus.outcome.avg) {
+  if (clus.tree.growing & clus.outcome.avg) {
     res <- tibble(!!(id):=as.character(pull(data.validation, id)),
                   cluster=as.character(clus[ls.forest$trt.dof]),
                   !!(paste0(y, ".pred")):=as.numeric(ls.forest$Y.pred))
@@ -125,7 +158,7 @@ growForest <- function(data.trainest, data.validation, y, id, trt, vars, prob,
 }
 
 oracle <- function(data, y, id, trt) {
-  trts <- data %>% pull(trt) %>% unique
+  trts <- data %>% pull(!!sym(trt)) %>% unique
   data %>%
     dplyr::select(all_of(c(id, paste0(y, trts)))) %>%
     pivot_longer(cols=paste0(y, trts), names_to=trt, names_prefix=y,
