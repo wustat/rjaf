e8acaf461b6d942e498988d2b2ff4df3803ea904
Wenbo Wu <wenbowu@wenbos-imac.policytech.com>
2024-03-19 09:37:05 -0400

    replace sim.data.R with the newest setting

--- R/rjaf.R
+++ R/rjaf.R
@@ -33,20 +33,22 @@
 #' deviation of the overall outcome `y`. The default value is 0.1.
 #' @param resid if `TRUE`, we implement the arbitrary residualization so the algorithm can choose baseline function that reduce the variance of the outcome. The default value is `TRUE`.
 #' @param clus.tree.growing if `TRUE`, the algorithm should perform tree growing based on clustering. The default value is `FALSE`.
-#' @param clus.outcome.avg if `TRUE`, the algorithm should calculate the average outcome within each cluster. The default value is `FALSE`.
+#' @param clus.outcome.avg if `TRUE`, the algorithm should calculate the average
+#' outcome within each cluster. This option is deprecated. The default value is `FALSE`.
 #' @param clus.max control the maximum number of clusters when performing k-means clustering.
-#' It should be greater than 1 and less than or equal to the number of unique treatments `length(trts)`. The default value is 10.
+#' It should be greater than 1 and less than or equal to the number of unique treatments `length(trts)`.
+#' The default value is 10.
 #' @param reg if `TRUE`, we grow the regularized version of the joint assignment forest. 
 #' This parameter is passed to the function within `rjaf_cpp` function in the `rjaf.cpp` file.
-#' @param impute if `TRUE`, imputation is used to grow tree under regularization. This parameter is passed to the rjaf_cpp function in the rjaf.cpp file. 
-#' @param setseed if `TRUE`,  value of `seed` is passed to `rjaf.cpp` file and function `set_seed()` that sets the random seed in R.
-#' @param seed value used to set seed in `rjaf.cpp` `set_seed()` function is setseed is `TRUE`. The default value is 1
+#' @param impute if `TRUE`, imputation is used to grow tree under regularization. This parameter is passed to the `rjaf_cpp` function in the `rjaf.cpp` file. 
+#' @param setseed if `TRUE`,  value of `seed` is passed to `rjaf.cpp` file and function `set_seed()` that sets the random seed in `R`.
+#' @param seed value used to set seed in `rjaf.cpp` `set_seed()` function is setseed is `TRUE`. The default value is 1.
 #' @param nfold number of folds in cross-validation to choose the combination of these parameters for each arm-“noise” setting
 #' 
 #' 
 #' @return If both `clus.tree.growing` and `clus.outcome.avg` are true, return list containing a tibble (named as "res") with ID, cluster number, 
-#' and predicted outcome, and a dataframe (named as "clustering") with cluster number, probability of being assigned to the cluster, and treatment.
-#' If not, return a tibble with ID, treatment predicted by the directed optimization forest, predicted outcome, and corresponding treatment outcome from the validation data.
+#' and predicted outcome, and a data frame (named as "clustering") with cluster number, probability of being assigned to the cluster, and treatment.
+#' If not, return a tibble with ID, treatment predicted by the regularized joint assignment forest, predicted outcome, and corresponding treatment outcome from the validation data.
 
 #' @export 
 #' 
--- R/sim.data.R
+++ R/sim.data.R
@@ -9,22 +9,22 @@ sim.data <- function(n, K, gamma, sigma, count=rep(1,K+1)) {
   invisible(sapply(0:K, function(t) data[data$cl==t, "cid"] <<-
                      sample(count[t+1], sum(data$cl==t), T, rep(1, count[t+1]))))
   data <- data %>%
-    mutate(tmp1=10+20*(X1>0)-20*(X2>0)-40*(X1>0&X2>0)+1,
+    mutate(tmp1=10+20*(X1>0)-20*(X2>0)-40*(X1>0&X2>0),
            tmp2=gamma*(2*(X3>0)-1)/(K-1),
-           tmp3=-X1^2,
+           tmp3=-10*X1^2,
            Y=tmp1+tmp2*(cl>0)*(2*cl-K-1)+tmp3*(cl==0)+rnorm(n,0,sigma),
            trt=str_c("c", cl, "t", cid))
   mapping <- data %>% distinct(trt, .keep_all=T) %>%
     select(c(cl, cid, trt)) %>% arrange(trt)
   # Y: observed outcomes
   Y.cf.trt <- data.frame(sapply(mapping %>% pull(trt), function(t) {
-    # counterfactual outcomes
+    # counterfactural outcomes
     clus <- mapping %>% filter(trt==t) %>% pull(cl)
     mutate(data, Y=tmp1+tmp2*(clus>0)*(2*clus-K-1)+tmp3*(clus==0))$Y
   }))
   names(Y.cf.trt) <- paste0("Y", mapping %>% pull(trt))
   Y.cf.cl <- data.frame(sapply(0:K, function(t) {
-    # counterfactual outcomes
+    # counterfactural outcomes
     mutate(data, Y=tmp1+tmp2*(t>0)*(2*t-K-1)+tmp3*(t==0))$Y
   }))
   names(Y.cf.cl) <- paste0("Y",0:K)
