54faf46e5c09a1f10ac2cd8cbb27b9e848050dc0
Xinyi Zhang <zhangxinyi@ufl.edu>
2024-04-28 15:52:45 -0400

    fix data.R documentation

--- R/data.R
+++ R/data.R
@@ -9,12 +9,24 @@
 #'   \item{id}{Observation ID}
 #'   \item{cl}{Assigned cluster number}
 #'   \item{cid}{Cluster-specific identifier for each observation within its cluster. It is assigned based on the cluster assignment ``cl`` and the ``count`` parameter.}
-#'   \item{X1...X3}{Covariates with each observation}
+#'   \item{X1}{Covariate X1}
+#'   \item{X2}{Covariate X2}
+#'   \item{X3}{Covariate X3}
 #'   \item{prob}{Probability of treatment assignment}
 #'   \item{Y}{Observed outcomes}
 #'   \item{trt}{Treatment assignment for each observation}
-#'   \item{Yc0t1...Yc5t1}{Counterfactual outcome for the scenario where an observation belongs to cluster ``c`` and is assigned to treatment ``t``.}
-#'   \item{Y0...Y5}{Counterfactual outcome for the scenario where an observation is assigned to a specific treatment}
+#'   \item{Yc0t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 0 and is assigned to treatment 1}
+#'   \item{Yc1t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 1 and is assigned to treatment 1}
+#'   \item{Yc2t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 2 and is assigned to treatment 1}
+#'   \item{Yc3t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 3 and is assigned to treatment 1}
+#'   \item{Yc4t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 4 and is assigned to treatment 1}
+#'   \item{Yc5t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 5 and is assigned to treatment 1}
+#'   \item{Y0}{Counterfactual outcome for the scenario where an observation is assigned to treatment 0}
+#'   \item{Y1}{Counterfactual outcome for the scenario where an observation is assigned to treatment 1}
+#'   \item{Y2}{Counterfactual outcome for the scenario where an observation is assigned to treatment 2}
+#'   \item{Y3}{Counterfactual outcome for the scenario where an observation is assigned to treatment 3}
+#'   \item{Y4}{Counterfactual outcome for the scenario where an observation is assigned to treatment 4}
+#'   \item{Y5}{Counterfactual outcome for the scenario where an observation is assigned to treatment 5}
 #' }
 "Example_data"
 
@@ -30,11 +42,24 @@
 #'   \item{id}{Bbservation ID}
 #'   \item{cl}{Assigned cluster number}
 #'   \item{cid}{Cluster-specific identifier for each observation within its cluster. It is assigned based on the cluster assignment ``cl`` and the ``count`` parameter.}
-#'   \item{X1...X3}{Covariates with each observation}
+#'   \item{X1}{Covariate X1}
+#'   \item{X2}{Covariate X2}
+#'   \item{X3}{Covariate X3}
 #'   \item{prob}{Probability of treatment assignment}
 #'   \item{Y}{Observed outcomes}
 #'   \item{trt}{Treatment assignment for each observation}
-#'   \item{Y0...Y5}{Counterfactual outcome for the scenario where an observation is assigned to a specific treatment}
+#'   \item{Yc0t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 0 and is assigned to treatment 1}
+#'   \item{Yc1t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 1 and is assigned to treatment 1}
+#'   \item{Yc2t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 2 and is assigned to treatment 1}
+#'   \item{Yc3t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 3 and is assigned to treatment 1}
+#'   \item{Yc4t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 4 and is assigned to treatment 1}
+#'   \item{Yc5t1}{Counterfactual outcome for the scenario where an observation belongs to cluster 5 and is assigned to treatment 1}
+#'   \item{Y0}{Counterfactual outcome for the scenario where an observation is assigned to treatment 0}
+#'   \item{Y1}{Counterfactual outcome for the scenario where an observation is assigned to treatment 1}
+#'   \item{Y2}{Counterfactual outcome for the scenario where an observation is assigned to treatment 2}
+#'   \item{Y3}{Counterfactual outcome for the scenario where an observation is assigned to treatment 3}
+#'   \item{Y4}{Counterfactual outcome for the scenario where an observation is assigned to treatment 4}
+#'   \item{Y5}{Counterfactual outcome for the scenario where an observation is assigned to treatment 5}
 #' }
 "Example_data_clus"
 
@@ -47,13 +72,18 @@
 #' @format A data frame with 100 rows and 13 columns 
 #' \describe{
 #'   \item{id}{Bbservation ID}
-#'   \item{cl}{Assigned cluster number}
-#'   \item{cid}{Cluster-specific identifier for each observation within its cluster. It is assigned based on the cluster assignment ``cl`` and the ``count`` parameter.}
-#'   \item{X1...X3}{Covariates with each observation}
+#'   \item{trt}{Treatment assigned for each observation}
+#'   \item{X1}{Covariate X1}
+#'   \item{X2}{Covariate X2}
+#'   \item{X3}{Covariate X3}
 #'   \item{prob}{Probability of treatment assignment}
 #'   \item{Y}{Observed outcomes}
-#'   \item{trt}{Treatment assignment for each observation}
-#'   \item{Y0...Y5}{Counterfactual outcome for the scenario where an observation is assigned to a specific treatment}
+#'   \item{Y0}{Counterfactual outcome for the scenario where an observation is assigned to treatment 0}
+#'   \item{Y1}{Counterfactual outcome for the scenario where an observation is assigned to treatment 1}
+#'   \item{Y2}{Counterfactual outcome for the scenario where an observation is assigned to treatment 2}
+#'   \item{Y3}{Counterfactual outcome for the scenario where an observation is assigned to treatment 3}
+#'   \item{Y4}{Counterfactual outcome for the scenario where an observation is assigned to treatment 4}
+#'   \item{Y5}{Counterfactual outcome for the scenario where an observation is assigned to treatment 5}
 #' }
 "Example_data_non_clus"
 
